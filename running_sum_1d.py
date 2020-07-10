99ed3f356dff733dc19446640eba7a6530a92d43
Robert Carter <rc3331@gmail.com>
2020-07-09 22:26:53 -0500

    added running_sum_1d.py

---
+++ running_sum_1d.py
@@ -0,0 +1,19 @@
+"""
+This problem asks to keep a running total of the sum of a 1 dimensional array, and add that sum to each item in the array as we traverse.
+
+To do this, we can simply declare a variable, running sum, and add it to each item in the array as we traverse. We need to add the sum to the item in the array, while storing the original value of the item in a temporary
+variable to add to the running sum.
+
+Example output [1, 2, 3, 4] --> [1, 3, 6, 10]
+"""
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        running_sum = 0
+        
+        for i in range(len(nums)):
+            temp = nums[i]
+            nums[i] += running_sum
+            running_sum += temp
+        
+        return nums
